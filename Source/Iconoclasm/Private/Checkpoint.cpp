c0dc5cce2e3413dea77bf4660ee71ac466f58c4e
Cade Gordon <cadegordon093@gmail.com>
2025-07-22 22:15:39 -0500

    checkpoints kills ads again

--- Source/Iconoclasm/Private/Checkpoint.cpp
+++ Source/Iconoclasm/Private/Checkpoint.cpp
@@ -5,6 +5,11 @@
 #include "Components/BoxComponent.h"
 #include "IconoclasmCharacter.h"
 #include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "RangedEnemyCharacter.h"
+#include "FlyingEnemyCharacter.h"
+#include "GruntEnemyCharacter.h"
+#include "RaphaelBossCharacter.h"
 
  
 
@@ -35,5 +40,52 @@ void ACheckpoint::OnCheckpointOverlap(UPrimitiveComponent* OverlappedComponent, 
 	if (PlayerCharacter)
 	{
 		PlayerCharacter->SetCheckpointLocation(GetActorLocation());
+
+		DestroyEnemyCharacters();
+	}
+}
+
+void ACheckpoint::DestroyEnemyCharacters()
+{
+	UWorld* World = GetWorld();
+	if (!World)
+	{
+		return;
+	}
+
+	// Destroy RangedEnemyCharacters (except Raphael boss)
+	TArray<AActor*> RangedEnemies;
+	UGameplayStatics::GetAllActorsOfClass(World, ARangedEnemyCharacter::StaticClass(), RangedEnemies);
+	for (AActor* Enemy : RangedEnemies)
+	{
+		ARaphaelBossCharacter* RaphaelBoss = Cast<ARaphaelBossCharacter>(Enemy);
+		if (!RaphaelBoss)
+		{
+			Enemy->Destroy();
+		}
+	}
+
+	// Destroy FlyEnemyCharacters (except Raphael boss)
+	TArray<AActor*> FlyEnemies;
+	UGameplayStatics::GetAllActorsOfClass(World, AFlyingEnemyCharacter::StaticClass(), FlyEnemies);
+	for (AActor* Enemy : FlyEnemies)
+	{
+		ARaphaelBossCharacter* RaphaelBoss = Cast<ARaphaelBossCharacter>(Enemy);
+		if (!RaphaelBoss)
+		{
+			Enemy->Destroy();
+		}
+	}
+
+	// Destroy GruntEnemyCharacters (except Raphael boss)
+	TArray<AActor*> GruntEnemies;
+	UGameplayStatics::GetAllActorsOfClass(World, AGruntEnemyCharacter::StaticClass(), GruntEnemies);
+	for (AActor* Enemy : GruntEnemies)
+	{
+		ARaphaelBossCharacter* RaphaelBoss = Cast<ARaphaelBossCharacter>(Enemy);
+		if (!RaphaelBoss)
+		{
+			Enemy->Destroy();
+		}
 	}
 }
--- Source/Iconoclasm/Public/Checkpoint.h
+++ Source/Iconoclasm/Public/Checkpoint.h
@@ -6,6 +6,13 @@
 #include "GameFramework/Actor.h"
 #include "Checkpoint.generated.h"
 
+class UBoxComponent;
+class AIconoclasmCharacter;
+class ARangedEnemyCharacter;
+class AFlyingEnemyCharacter;
+class AGruntEnemyCharacter;
+class ARaphaelBossCharacter;
+
 UCLASS()
 class ICONOCLASM_API ACheckpoint : public AActor
 {
@@ -25,5 +32,8 @@ protected:
 	UFUNCTION()
 	void OnCheckpointOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	void DestroyEnemyCharacters();
 
 };
